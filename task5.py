048505219538a5bd3cf8fb8ad7256ce8a9aecb21
Sebkd <sebkd@mail.ru>
2020-12-30 08:46:03 +0700

    coding task5

--- task5.py
+++ task5.py
@@ -5,11 +5,17 @@
 # выполнение программы завершается. Если специальный символ введен после нескольких чисел,
 # то вначале нужно добавить сумму этих чисел к полученной ранее сумме и после этого завершить программу.
 def my_func_summ (*args):
-    for index_number in list(args).copy():
-        for i in index_number
-        return sum(index_number)
+    index_number = args[0]
+    for index in index_number:
+        if (str(index).isdigit()) == False:
+            print(type(index), index)
+            return 0
+
+        return sum(map(int, index_number))
+
 
 def memory_save_summ (*args):
+    num_list = []
     pass
 
 
@@ -17,17 +23,11 @@ def memory_save_summ (*args):
 
 save_memory = 0
 while True:
-    try:
-        in_data = list(map(int, input('Введите через пробел любые числа\n').split()))
+    # try:
+        in_data = list(input('Введите через пробел любые числа\n').split())
         save_memory = save_memory + my_func_summ(in_data)
         print(f'Сумма числе равна {save_memory}')
         continue
-    # except IndexError:
-    #     print('Вы ввели меньше 2, попробуйте еще')
-    #     continue
     except ValueError:
         print('Вы ввели не числа, попробуйте еще')
         continue
-    # except ZeroDivisionError:
-    #     print('Одно из чисел равно 0, попробуйте еще')
-    #     continue
