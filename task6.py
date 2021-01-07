762ab3c996e8f140082f1f473dd0b3c7beb46c9f
Sebkd <sebkd@mail.ru>
2021-01-07 15:01:42 +0700

    final task6

--- task6.py
+++ task6.py
@@ -4,9 +4,32 @@
 # Каждое слово состоит из латинских букв в нижнем регистре. Сделать вывод исходной строки,
 # но каждое слово должно начинаться с заглавной буквы. Необходимо использовать написанную ранее функцию int_func().
 
-def int_func(text):
+def int_func(*args):
+    local_string = args[0].split()
+    my_list = []
+    my_str = ''
+    for index in local_string:
+        my_list.append(index.capitalize())
+        my_str += my_list.pop() + (' ' if len(local_string) > 1 else '')
+    return my_str
+
+def int_func_two(*args):
+    local_string = args[0].split()
+    my_list = []
+    for index in local_string:
+        my_list.append(index.capitalize())
+    return ' '.join(my_list)
+
+def int_func_three(text):
     return text.title()
 
 text = 'text'
 print(f'{text} до преобразования')
 print(f'{int_func(text)} после вызова функции')
+print(f'{int_func_two(text)} после вызова функции two')
+print(f'{int_func_three(text)} после вызова функции three')
+text = 'text is text'
+print(f'{text} до преобразования')
+print(f'{int_func(text).rstrip()} после вызова функции')
+print(f'{int_func_two(text)} после вызова функции two')
+print(f'{int_func_three(text)} после вызова функции three')
